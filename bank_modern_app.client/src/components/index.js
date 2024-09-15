62822eedd36f823513af45f71a5bc73874fd533c
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-09-15 10:45:39 -0400

    creating basic structure of the web

---
+++ bank_modern_app.client/src/components/index.js
@@ -0,0 +1,28 @@
+import Billing from "./Billing/Billing";
+import Business from "./Business/Business";
+import Button from "./Button/Button";
+import CardDeal from "./CardDeal/CardDeal";
+import Clients from "./Clients/Clients";
+import CTA from "./CTA/CTA";
+import FeedbackCard from "./FeedbackCard/FeedbackCard";
+import Footer from "./Footer/Footer";
+import GetStarted from "./GetStarted/GetStarted";
+import Hero from "./Hero/Hero";
+import Navbar from "./Navbar/Navbar";
+import Stats from "./Stats/Stats";
+import Testimonials from "./Testimonials/Testimonials";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  FeedbackCard,
+  Footer,
+  GetStarted,
+  Hero,
+  Stats,
+  Testimonials,
+};
