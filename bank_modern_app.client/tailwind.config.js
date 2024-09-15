80ce9fc957b47432f1cba8b8554d594f1ec690e5
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-09-15 17:11:48 -0400

    build(navbar): implementing navbar component

--- bank_modern_app.client/tailwind.config.js
+++ bank_modern_app.client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   mode: "jit",
   theme: {
     extend: {
